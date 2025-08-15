6c1400eb8e287b284ed40db74f296f4001a00370
Carlos Lopez <carlosdaniel.lg@gmail.com>
2025-08-15 07:30:25 +0000

    datasetts

--- js/appboard.js
+++ js/appboard.js
@@ -24,23 +24,14 @@ const UPDATE_INTERVALS = {
 const DATA_URLS = {
   sequence: { // Datos de secuencia para máquinas específicas
     A: "datasets/test_A.xml",
-<<<<<<< HEAD
+
     B: "datasets/test_B.xml",
     //C: "http://10.2.160.72:8081/xml"
+  
     C: "datasets/test_C.xml"
-    //C: "http://10.2.160.72:8081/xml"
-   
-=======
-<<<<<<< HEAD
-    B: "datasets/test_B.xml",
-    //C: "http://10.2.160.72:8081/xml"
-    C: "datasets/test_C.xml"
-=======
-    B: "test_B.xml",
-    C: "http://10.2.160.72:8081/xml"
-    //C: "test_C.xml"
->>>>>>> 46bf6245a38ba5ebfc24278a8dd70c38889914dc
->>>>>>> 9860a5164e239723ef98c249dbb70664c9d76a9e
+
+  
+
   },
   production: "datasets/production_data.xml" // Datos de producción para todas las máquinas
 };
